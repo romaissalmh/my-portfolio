6780e72a738d673d39ff6229a7917e776e996b27
romaissalmh <hr_kessi@esi.dz>
2021-09-18 22:36:35 +0100

    fixed some css issues

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -29,73 +29,72 @@ export default function Skills() {
             <Img className="img" atl="certifications" fluid={fluid} style={{padding:"-40px" }}  />
 
             <div style={{marginTop:"4rem"}}>
-                <div className="column">
 
-                    <div className="row">
                     <br/> 
-                        <div style={{display:"block"}} >
+                    <div className="grid-container">
+
+                        <div className="grid-item">
                             <DiHtml5 className="icon"/>  
                             <h4 className="skills-titles" > HTML5</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <DiCss3 className="icon"/>  
                             <h4 className="skills-titles" > CSS3</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <DiBootstrap className="icon"/>  
                             <h4 className="skills-titles" > Bootstrap</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                       
+
+                        <div className="grid-item">
                             <DiGithubBadge className="icon"/>  
                             <h4 className="skills-titles" > Github</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <DiPhp className="icon"/>  
                             <h4 className="skills-titles" > PHP</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <DiJavascript1 className="icon"/>  
                             <h4 className="skills-titles" > Javascript</h4>  
                         </div>
-                    </div>
-                    <br/>   <br/>  <br/> 
 
-                    <div className="row">
+                  
 
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <DiNodejsSmall className="icon"/>  
                             <h4 className="skills-titles" > Node.js</h4>  
-                        </div>
-                        <div style={{display:"block"}} >
+                        </div> 
+                        <div className="grid-item">
                             <FaJava className="icon"/>  
                             <h4 className="skills-titles" > Java</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <FaJenkins className="icon"/>  
                             <h4 className="skills-titles" > Jenkins</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <DiMysql className="icon"/>  
                             <h4 className="skills-titles" > MySQL</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <DiMongodb className="icon"/>  
                             <h4 className="skills-titles" > MongoDB</h4>  
                         </div>
-                        <div style={{display:"block"}} >
+                        <div className="grid-item">
                             <DiPostgresql className="icon"/>  
                             <h4 className="skills-titles" > PostgreSQL</h4>  
                         </div>
-                    </div> <br/>  <br/> 
+                 
 
-                    <div className="row">
-                        <div style={{display:"block"}} >
-                            <SiKotlin style={{ width:"80px", height:"80px", marginLeft:"1rem"}} className="icon"/>  
+                        <div >
+                            <SiKotlin style={{ marginLeft:"4rem"}} className="icon"/>  
                             <h4 className="skills-titles" > Kotlin</h4>  
                         </div>
                     </div>
 
-                </div>
+            
 
             </div>
             </div>  
