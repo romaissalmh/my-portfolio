4764090688459f9a5c5cf4007527e5a4f1d95636
romaissalmh <hr_kessi@esi.dz>
2021-09-19 16:54:45 +0100

    changed aos config

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,32 @@ import Header from "../components/Header"
 import Layout from "../components/Layout"
 import Projects from "../components/Projects"
 import Skills from "../components/Skills"
-import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 
-AOS.init({
-    once: false, // whether animation should happen only once - while scrolling down
-    offset:30,
-    duration:1000
 
-});
 
 export default function Home() {
+    let AOS;
+
+    useEffect(() => {
+        /**
+         * Server-side rendering does not provide the 'document' object
+         * therefore this import is required either in useEffect or componentDidMount as they
+         * are exclusively executed on a client
+         */
+        const AOS = require("aos");
+        AOS.init({
+            once: false, // whether animation should happen only once - while scrolling down
+            offset:30,
+            duration:1000
+        });
+      }, []);
+    
+      useEffect(() => {
+        if (AOS) {
+          AOS.refresh();
+        }
+      });
   return (
   <Layout>
       <Header/>
